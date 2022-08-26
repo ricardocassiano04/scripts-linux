60ca66a27957b6e5727b5df4836c8ff025cb93bb
Ricardo Cassiano <rc.cassiano04@gmail.com>
2022-08-26 16:16:38 -0300

    Development packages

---
+++ debian-development-packages.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Author: Ricardo Cassiano
+# E-mail: rc.cassiano04@gmail.com
+# Github: https://github.com/ricardocassiano04
+# 2022-08-26
+#
+# Install all packages for Python
+# and Java Developmento in Debian Gnu Linux 11.
+#
+# These are packates that I use for my own needs.
+# Feel free to modify them.
+# 
+#
+# TODO
+# Install  Java Language Server from Eclipse package (https://github.com/eclipse/eclipse.jdt.ls)
+
+
+INSTALAR_PACOTE="sudo apt-get -y install"
+REMOVER_PACOTE="sudo apt-get -y remove"
+
+
+# Refresh repositories
+
+sudo apt-get update
+
+# Java and Python development packages (including Python Language Server)
+
+$INSTALAR_PACOTE 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
