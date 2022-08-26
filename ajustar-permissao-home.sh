7cadcd43d3860712c2f8b437ee236d57082a7512
Ricardo Cassiano <rc.cassiano04@gmail.com>
2022-08-26 17:36:13 -0300

    Update ajustar-permissao-home.sh

--- ajustar-permissao-home.sh
+++ ajustar-permissao-home.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 #
+# Autor: Ricardo Cassiano
+# E-mail: rc.cassiano04@outlook.com
+#
+# Limitar permissão de leitura e escrita de todos os diretórios
+# da pasta $HOME  e de alguns arquivos (ver as extensões na linha 23)
+# para somente o usuário atual.
 #
 #
-#
+
+
 
 sudo chown  -R $USER:$USER $HOME
 
@@ -13,9 +20,13 @@ chmod --change 700 $HOME
 find $HOME -type d -exec chmod --change 700 {} \;
 
 
+extensoes=(
+sql xls xlsx doc docx \
+py java deb rpm gz tar zip \
+rar png jpeg jpeg zst
+)
 
 
-
-
-
-
+for ajustar_permissoes in "${extensoes[@]}"; do
+find $HOME -type f -iname *.$ajustar_permissoes -exec chmod --change 600 {} \;
+; done
