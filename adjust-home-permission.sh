4a4ff056d4199298a81eeb478ea3f770aea2bd36
Ricardo Cassiano <rc.cassiano04@gmail.com>
2022-09-16 17:09:27 -0300

    Neovim install addeded

--- adjust-home-permission.sh
+++ adjust-home-permission.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 #
-# Autor: Ricardo Cassiano
-# E-mail: rc.cassiano04@outlook.com
-#
-# Limitar permissão de leitura e escrita de todos os diretórios
-# da pasta $HOME  e de alguns arquivos (ver as extensões na linha 23)
-# para somente o usuário atual.
-#
-#
-
+# Author: Ricardo Cassiano
+# 
+# Script to adjust permissions of $HOME and its subdirectories
+# and some files with some extensions.
+# It allow only the current user to read/writer $HOME
+# 
 
 
 sudo chown  -R $USER:$USER $HOME
 
-
 chmod --change 700 $HOME
-
 
 find $HOME -type d -exec chmod --change 700 {} \;
 
-
-extensoes=(
+extensions=(
 sql xls xlsx doc docx \
 py java deb rpm gz tar zip \
 rar png jpeg jpeg zst
 )
 
-
-for ajustar_permissoes in "${extensoes[@]}"; do
-find $HOME -type f -iname *.$ajustar_permissoes -exec chmod --change 600 {} \;
+for adjust_permissions in "${extensions[@]}"; do
+find $HOME -type f -iname *.$adjust_permissions -exec chmod --change 600 {} \;
 ; done
---
+++ neovim-install-from-source.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Download and build Neovim from # https://github.com/neovim/neovim on Debian/Ubuntu.
+#
+#
+# This script is based on  Neovim wiki: https://github.com/neovim/neovim/wiki/Installing-Neovim#install-from-source
+# with a few changes to fit my own needs.
+
+
+sudo apt-get -y install ninja-build gettext libtool libtool-bin \
+autoconf automake cmake g++ pkg-config unzip curl doxygen
+
+
+git clone https://github.com/neovim/neovim
+
+cd neovim || return
+
+git checkout stable
+
+make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/apps/neovim"
+make install
+
+tee -a "$HOME"/.profile>>/dev/null<<EOF
+export PATH="$HOME/apps/neovim/bin:$PATH"
+EOF
+
+
+echo "Installation finished!"
