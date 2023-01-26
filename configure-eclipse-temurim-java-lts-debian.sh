238bd6689520ba73b7907a6d65bf19fccf722262
Ricardo Cassiano <rc.cassiano04@gmail.com>
2023-01-26 16:58:40 -0300

    A few changes

---
+++ configure-eclipse-temurim-java-lts-debian.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Author: Ricardo Cassiano
+#
+# Configure Eclipse Temurin OpenJDK on Debian
+#
+
+
+read -p -r "Type your desired jdk version (e.g: 8, 11 , 17): ": VERSION
+
+
+echo "You can download Eclipse Temurin OpenJDK  binaries here:
+
+https://adoptium.net/
+
+This script assumes that you have downloaded jdk and 
+moved it to /opt/jdk$VERSION folder.
+
+Now your jdk $VERSION will be the default java on the system."
+
+sleep 2
+
+binaries=(
+  jar jarsigner java javac javadoc \
+  javap jcmd jconsole jdb jdeprscan \
+  jdeps jfr jhsdb jimage jinfo jlink \
+  jmap jpackage jps jrunscript jshell \
+  jstack jstat jstatd keytool rmiregistry \
+  serialver
+  )
+
+for update_binaries in "${binaries[@]}"; do sudo update-alternatives --install \
+/usr/bin/"$update_binaries" "$update_binaries" /opt/"$VERSION"/bin/"$update_binaries" 100 && \
+sudo update-alternatives --set "$update_binaries" /opt/"$VERSION"/bin/"$update_binaries"; done
+
+echo "Now Java $VERSION is the system wide default!"
+
+java -version
