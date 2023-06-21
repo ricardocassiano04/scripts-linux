9377002ca2f806dccd2c87a8f59754268c94cb8f
Ricardo Cassiano <rc.cassiano04@gmail.com>
2023-06-20 21:46:56 -0300

    Adjust packages for Debian 12 Bookworm version

--- debian-development-packages.sh
+++ debian-development-packages.sh
@@ -9,9 +9,9 @@
 sudo apt-get update
 
 sudo apt-get -y install default-{jdk,jdk-doc} visualvm \
-python3-{pyls,pyls-black,rope,pyflakes,mccabe,jedi} \
+python3-{pylsp,pylsp-black,rope,pyflakes,mccabe,jedi} \
 python3-{pycodestyle,pydocstyle,yapf} pylint flake8 \
 python3-{dev,pip,wheel,rope} \
 bison flex llvm clang zlib1g-dev \
 lib{ssl,systemd,readline,xslt1,xml2}-dev m4 make autoconf \
-pkgconf flex gcc make guile-2.2-dev patch automake  python3-dev
+pkgconf flex gcc make guile-3.0-dev patch automake  python3-dev
