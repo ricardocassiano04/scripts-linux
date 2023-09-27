7b3759e16d19688be45c821d135d07787ef3bc4c
Ricardo Cassiano <rc.cassiano04@gmail.com>
2023-09-26 22:13:21 -0300

    manjaro linux adjustments

---
+++ manjaro-linux-adjustments.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Author: Ricardo Cassiano
+#
+#
+# Script for  Manjaro Linux adjustments
+
+
+# Adjust breeze gtk 4 theme 
+
+ln -s /usr/share/themes/Breeze/gtk-4.0/{gtk.css,gtk-dark.css} ~/.config/gtk-4.0/
+ln -s /usr/share/themes/Breeze/assets ~/.config/gtk-4.0/
