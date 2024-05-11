ee67ec3a403ff08f94a93ac308ca682a4071714b
ricardo <ricardo@localhost.localdomain>
2024-05-11 20:00:59 -0300

    Script for some linux adjustments

--- linux-adjustments.sh
+++ linux-adjustments.sh
@@ -3,10 +3,14 @@
 # Author: Ricardo Cassiano
 #
 #
-# Script for  Manjaro Linux adjustments
+# Script for some adjustments in Linux
 
 
 # Adjust breeze gtk 4 theme 
+
+rm -rf ~/.config/gtk-4.0/*.css
+rm -rf ~/.config/gtk-4.0/assets
+
 
 ln -s /usr/share/themes/Breeze/gtk-4.0/{gtk.css,gtk-dark.css} ~/.config/gtk-4.0/
 ln -s /usr/share/themes/Breeze/assets ~/.config/gtk-4.0/
