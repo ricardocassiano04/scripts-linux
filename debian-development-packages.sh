f5b4367a3164088fed5b78e94d5f1dbb0b627f55
Ricardo Cassiano <rc.cassiano04@gmail.com>
2024-05-13 21:34:06 -0300

    Remove java packages because there a separated script for it

--- debian-development-packages.sh
+++ debian-development-packages.sh
@@ -2,7 +2,7 @@
 #
 # Author: Ricardo Cassiano
 # 
-# Install some packages for Java and Python in Debian.
+# Install some packages for development in Debian.
 #
 
 
@@ -10,7 +10,7 @@
 
 sudo apt-get update
 
-sudo apt-get -y install default-{jdk,jdk-doc} visualvm \
+sudo apt-get -y install  \
 python3-{pylsp,pylsp-black,rope,pyflakes,mccabe,jedi} \
 python3-{pycodestyle,pydocstyle,yapf} pylint flake8 \
 python3-{dev,pip,wheel,rope} \
@@ -18,3 +18,4 @@ bison flex llvm clang zlib1g-dev \
 lib{ssl,systemd,readline,xslt1,xml2}-dev m4 make autoconf \
 pkgconf flex gcc make guile-3.0-dev patch automake  python3-dev \
 python3-full
+
