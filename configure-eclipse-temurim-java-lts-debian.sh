348cf87b52d9da1aa64c49245950dad67e16583a
Ricardo Cassiano <rc.cassiano04@gmail.com>
2024-05-22 21:23:43 -0300

    Solve a codename issue in Linux Mint

--- configure-eclipse-temurim-java-lts-debian.sh
+++ configure-eclipse-temurim-java-lts-debian.sh
@@ -55,8 +55,16 @@ else
   sudo apt-get install -y wget apt-transport-https gpg
 
   wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
+  
+  # Get distro codename
 
-  echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
+    if [ "$(grep -E '^ID=' /etc/os-release)" = "ID=linuxmint" ]; then
+	    distro=$(grep -Po '(?<=UBUNTU_CODENAME=)\w+' /etc/os-release)	
+    else
+        distro=$(lsb_release -cs)   
+    fi
+
+  echo "deb https://packages.adoptium.net/artifactory/deb "${distro}" main" | sudo tee /etc/apt/sources.list.d/adoptium.list
 
   sudo apt-get update 
 
