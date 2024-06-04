678f1bf5b23784765bb7dace8be19583e4e9e5f0
Ricardo Cassiano <rc.cassiano04@gmail.com>
2024-06-03 22:32:59 -0300

    Atualização de versão no upstream

--- hblock-install.sh
+++ hblock-install.sh
@@ -10,14 +10,14 @@
 #
 
 
-curl -o /tmp/hblock 'https://raw.githubusercontent.com/hectorm/hblock/v3.4.4/hblock' \
-  && echo '7ad94c8525e6076c56b9209ed73d132b8b9600a2e5e9738b3cdc02839d6d4215  /tmp/hblock' | shasum -c \
+curl -o /tmp/hblock 'https://raw.githubusercontent.com/hectorm/hblock/v3.4.5/hblock' \
+  && echo '2db6f52a14a3b87ff86b0fde3583f1a635ff580d26b36f8a103997a614314c6f  /tmp/hblock' | shasum -c \
   && sudo mv /tmp/hblock /usr/local/bin/hblock \
   && sudo chown 0:0 /usr/local/bin/hblock \
   && sudo chmod 755 /usr/local/bin/hblock
 
 
-curl -o '/tmp/hblock.#1' 'https://raw.githubusercontent.com/hectorm/hblock/v3.4.4/resources/systemd/hblock.{service,timer}' \
+curl -o '/tmp/hblock.#1' 'https://raw.githubusercontent.com/hectorm/hblock/v3.4.5/resources/systemd/hblock.{service,timer}' \
   && echo '45980a80506df48cbfa6dd18d20f0ad4300744344408a0f87560b2be73b7c607  /tmp/hblock.service' | shasum -c \
   && echo '87a7ba5067d4c565aca96659b0dce230471a6ba35fbce1d3e9d02b264da4dc38  /tmp/hblock.timer' | shasum -c \
   && sudo mv /tmp/hblock.{service,timer} /etc/systemd/system/ \
@@ -36,6 +36,7 @@ sudo touch /etc/hblock/deny.list
 
 
 # Execute hblock script after download
-  hblock
+
+hblock
 
 
