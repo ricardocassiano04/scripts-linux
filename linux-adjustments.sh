ef9f5c8f3dd1096fdafcf935a1cf3d5987adb463
Ricardo Cassiano <rc.cassiano04@gmail.com>
2024-08-12 21:40:19 -0300

    Pequena correção

--- linux-adjustments.sh
+++ linux-adjustments.sh
@@ -3,10 +3,19 @@
 # Autor: Ricardo Cassiano
 #
 #
-# Script para configurações diversas no Linux
+# Configurações diversas no Linux
 
 
 # Ajuste do tema gtk 4  
 
-ln -f -s /usr/share/themes/Yaru-blue/gtk-4.0/{gtk.css,gtk-dark.css} ~/.config/gtk-4.0/
-ln -f -s /usr/share/themes/Yaru-blue-dark/assets ~/.config/gtk-4.0/
+
+ln -f -s /usr/share/themes/Matcha-dark-azul/gtk-4.0/{gtk.css,gtk-dark.css} ~/.config/gtk-4.0/
+
+
+# Desabilitar tracker no gnome
+
+systemctl --user mask tracker-xdg-portal-3 tracker-store.service \
+tracker-miner-fs.service tracker-miner-rss.service tracker-extract.service \
+tracker-miner-apps.service tracker-writeback.service tracker-xdg-portal-3.service
+
+
