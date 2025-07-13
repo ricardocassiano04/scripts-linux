5c0d20324f7506e77a9e9cacce5e891bafc6d9ae
ricardocassiano04 <rc.cassiano04@gmail.com>
2025-07-13 20:19:09 -0300

    Desabilitar o evolution no Gnome

--- linux-adjustments.sh
+++ linux-adjustments.sh
@@ -6,16 +6,27 @@
 # Configurações diversas no Linux
 
 
-# Ajuste do tema gtk 4  
+# Ajuste do tema gtk 4 caso não seja carregado corretamente.
+# Aqui no exemplo estou usando o tema Arc-Dark.
 
 
 ln -f -s /usr/share/themes/Arc-Dark/gtk-4.0/{gtk.css,gtk-dark.css} ~/.config/gtk-4.0/
 
 
-# Desabilitar tracker no gnome
+# Desabilitar alguns processos no gnome
 
 systemctl --user mask tracker-xdg-portal-3 tracker-store.service \
 tracker-miner-fs.service tracker-miner-rss.service tracker-extract.service \
 tracker-miner-apps.service tracker-writeback.service tracker-xdg-portal-3.service
+
+
+systemctl --user mask evolution-addressbook-factory.service
+systemctl --user mask evolution-calendar-factory.service
+systemctl --user mask evolution-source-registry.service
+
+
+
+rm -rf ~/.cache/tracker3
+rm -rf ~/.local/share/tracker3
 
 
