e49b10df1351abd7b7dfc9c78eaebd560356ccde
Ricardo Cassiano <rc.cassiano04@gmail.com>
2026-02-05 21:38:24 -0300

    Usar /usr/local/sbin ao invés de /usr/local/bin

--- comun/hblock-install.sh
+++ comun/hblock-install.sh
@@ -12,9 +12,9 @@
 
 curl -o /tmp/hblock 'https://raw.githubusercontent.com/hectorm/hblock/v3.5.1/hblock' \
   && echo 'd010cb9e0f3c644e9df3bfb387f42f7dbbffbbd481fb50c32683bbe71f994451  /tmp/hblock' | shasum -c \
-  && sudo mv /tmp/hblock /usr/local/bin/hblock \
-  && sudo chown 0:0 /usr/local/bin/hblock \
-  && sudo chmod 755 /usr/local/bin/hblock
+  && sudo mv /tmp/hblock /usr/local/sbin/hblock \
+  && sudo chown 0:0 /usr/local/sbin/hblock \
+  && sudo chmod 755 /usr/local/sbin/hblock
 
 
 curl -o '/tmp/hblock.#1' 'https://raw.githubusercontent.com/hectorm/hblock/v3.5.1/resources/systemd/hblock.{service,timer}' \
@@ -26,7 +26,7 @@ curl -o '/tmp/hblock.#1' 'https://raw.githubusercontent.com/hectorm/hblock/v3.5.
   && sudo systemctl daemon-reload \
   && sudo systemctl enable hblock.timer \
   && sudo systemctl start hblock.timer
-
+  
 
 sudo mkdir -p /etc/hblock/
 
