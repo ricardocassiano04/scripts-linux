e1877ced294786c41e5a47814f3a8de1cea520d0
Ricardo Cassiano <rc.cassiano04@gmail.com>
2026-02-18 22:20:03 -0300

    feat: verificação da versão do debian ao adicionar repositório do firefox

--- debian/debian-initial-setup.sh
+++ debian/debian-initial-setup.sh
@@ -2,7 +2,7 @@
 #
 # Autor: Ricardo Cassiano
 # 
-# Configurações iniciais do Linux Debian 12 Bookworm
+# Configurações iniciais do Linux Debian 12 trixie
 #
 
 
@@ -11,21 +11,26 @@
 # https://wiki.debian.org/SourcesList
 
 
+echo "Esse script é apenas para testes/estudos! Use por sua própria conta e risco!!"
+
+sleep 3
+
+
 sudo mv /etc/apt/source.list /etc/apt/source.list.backup
 
 sudo tee -a /etc/apt/source.list.d/debian.list>>/dev/null<<EOF
 
-deb https://deb.debian.org/debian bookworm main non-free-firmware
-deb-src https://deb.debian.org/debian bookworm main non-free-firmware
+deb https://deb.debian.org/debian trixie main non-free-firmware
+deb-src https://deb.debian.org/debian trixie main non-free-firmware
 
-deb https://security.debian.org/debian-security bookworm-security main non-free-firmware
-deb-src https://security.debian.org/debian-security bookworm-security main non-free-firmware
+deb https://security.debian.org/debian-security trixie-security main non-free-firmware
+deb-src https://security.debian.org/debian-security trixie-security main non-free-firmware
 
-deb https://deb.debian.org/debian bookworm-updates main non-free-firmware
-deb-src https://deb.debian.org/debian bookworm-updates main non-free-firmware
+deb https://deb.debian.org/debian trixie-updates main non-free-firmware
+deb-src https://deb.debian.org/debian trixie-updates main non-free-firmware
 
-deb https://deb.debian.org/debian bookworm-backports main contrib non-free non-free-firmware
-deb-src https://deb.debian.org/debian bookworm-backports main contrib non-free non-free-firmware
+deb https://deb.debian.org/debian trixie-backports main contrib non-free non-free-firmware
+deb-src https://deb.debian.org/debian trixie-backports main contrib non-free non-free-firmware
 
 EOF
 
@@ -42,7 +47,10 @@ sudo apt-get dist-upgrade -y
 ### Adicionar repositório do firefox
 # https://support.mozilla.org/pt-BR/kb/instale-o-firefox-no-linux#w_install-firefox-deb-package-for-debian-based-distributions
 
+source /etc/os-release
+
 echo "Agora será configurado o repositório oficial do Firefox e será instalado a versão mais recente."
+
 
 sudo install -d -m 0755 /etc/apt/keyrings
 
@@ -50,16 +58,40 @@ sudo apt-get -y install wget
 
 wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
 
+
+mkdir -p $HOME/.gnupg
+
 gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nO fingerprint da chave corresponde ("$0").\n"; else print "\nFalha na verificação: o fingerprint ("$0") não corresponde ao esperado.\n"}'
 
 
-echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
 
 echo '
 Package: *
 Pin: origin packages.mozilla.org
 Pin-Priority: 1000
 ' | sudo tee /etc/apt/preferences.d/mozilla
+
+
+  
+if [[ "$ID" == "debian"  ]]; then
+	versao=${VERSION_ID%%.*}
+	
+	if ((  versao < 13 )); then
+		echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+	elif (( versao > 12 )); then
+cat <<EOF | sudo tee /etc/apt/sources.list.d/mozilla.sources
+Types: deb
+URIs: https://packages.mozilla.org/apt
+Suites: mozilla
+Components: main
+Signed-By: /etc/apt/keyrings/packages.mozilla.org.asc
+EOF
+	fi
+else
+    echo "Versão do debian não suportada!!"
+    exit 0
+fi
+
 
 
 
@@ -139,7 +171,7 @@ EOF
 
 ### Instalar alguns pacotes do backports
 
-sudo apt-get -y -t bookworm-backports install libreoffice yt-dlp
+sudo apt-get -y -t trixie-backports install libreoffice yt-dlp
 
 
 
